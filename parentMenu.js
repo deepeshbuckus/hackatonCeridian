a5c3ee25fa9334eaeafd39d707040e4fb1a3d2dc
deepeshbuckus <deepesh.buckus@gmail.com>
2022-08-22 22:54:46 +0400

    drag and drop
    drag and drop

--- parentMenu.js
+++ parentMenu.js
@@ -1,154 +1,192 @@
 let parentMenus =
     [
         {
+            id: 1,
             "menu": "Analytics",
             Child: []
         },
         {
+            id: 2,
             "menu": "Anomaly Days",
             Child: []
         },
         {
+            id: 3,
             "menu": "Attendance Management",
             Child: []
         },
         {
+            id: 4,
             "menu": "Benefits",
             Child: []
         },
         {
+            id: 5,
             "menu": "Benefits Administration",
             Child: []
         },
         {
+            id: 6,
             "menu": "Benefits Intelligence",
             Child: []
         },
         {
+            id: 7,
             "menu": "Benefits Setup",
             Child: []
         },
         {
+            id: 8,
             "menu": "Career Profile Configuration",
             Child: []
         },
         {
+            id: 9,
             "menu": "Careers",
             Child: []
         },
         {
+            id: 10,
             "menu": "Centralized Schedules",
             Child: []
         },
         {
+            id: 11,
             "menu": "Compensation",
             Child: []
         },
         {
+            id: 12,
             "menu": "Compensation Administration",
             Child: []
         },
         {
+            id: 13,
             "menu": "Compensation Setup",
             Child: []
         },
         {
+            id: 14,
             "menu": "Data & Analytics",
             Child: []
         },
         {
+            id: 15,
             "menu": "Data Export Setup",
             Child: []
         },
         {
+            id: 15,
             "menu": "Documents",
             Child: []
         },
         {
+            id: 15,
             "menu": "Earnings",
             Child: []
         },
         {
+            id: 15,
             "menu": "Employee Timesheet",
             Child: []
         },
         {
+            id: 15,
             "menu": "Engagement",
             Child: []
         },
         {
+            id: 15,
             "menu": "Engagement Setup",
             Child: []
         },
         {
+            id: 15,
             "menu": "Getting Started",
             Child: []
         },
         {
+            id: 15,
             "menu": "HCM Anywhere",
             Child: []
         },
         {
+            id: 15,
             "menu": "Home",
             Child: []
         },
         {
+            id: 15,
             "menu": "HR Admin",
             Child: []
         },
         {
+            id: 15,
             "menu": "Learning",
             Child: []
         },
         {
+            id: 15,
             "menu": "Learning Setup",
             Child: []
         },
         {
+            id: 15,
             "menu": "Letter Management",
             Child: []
         },
         {
+            id: 15,
             "menu": "Manager Timesheet",
             Child: []
         },
         {
+            id: 15,
             "menu": "Market Pricing",
             Child: []
         },
         {
+            id: 15,
             "menu": "Mobile Org Schedules",
             Child: []
         },
         {
+            id: 15,
             "menu": "Multi-Week Calendar",
             Child: []
         },
         {
+            id: 15,
             "menu": "Onboarding",
             Child: []
         },
         {
+            id: 15,
             "menu": "Onboarding Setup",
             Child: []
         },
         {
+            id: 15,
             "menu": "Operation View",
             Child: []
         },
         {
+            id: 15,
             "menu": "Org Setup",
             Child: []
         },
         {
+            id: 15,
             "menu": "Pay Admin Checklist",
             Child: []
         },
         {
+            id: 15,
             "menu": "Pay Approve Checklist",
             Child: []
         },
         {
+            id: 15,
             "menu": "Pay Setup",
             Child: [
                 "Minimum Wages",
@@ -177,78 +215,97 @@ let parentMenus =
             ]
         },
         {
+            id: 15,
             "menu": "Payroll",
             Child: []
         },
         {
+            id: 15,
             "menu": "Payroll Import",
             Child: []
         },
         {
+            id: 15,
             "menu": "Payroll Setup",
             Child: []
         },
         {
+            id: 15,
             "menu": "Pension Auto-Enrollment",
             Child: []
         },
         {
+            id: 15,
             "menu": "Performance",
             Child: []
         },
         {
+            id: 15,
             "menu": "Performance Setup",
             Child: []
         },
         {
+            id: 15,
             "menu": "Period Review",
             Child: []
         },
         {
+            id: 15,
             "menu": "Plan",
             Child: []
         },
         {
+            id: 15,
             "menu": "Plan Admin Checklist",
             Child: []
         },
         {
+            id: 15,
             "menu": "Plan Setup",
             Child: []
         },
         {
+            id: 15,
             "menu": "PPACA",
             Child: []
         },
         {
+            id: 15,
             "menu": "Profile",
             Child: []
         },
         {
+            id: 15,
             "menu": "Projects",
             Child: []
         },
         {
+            id: 15,
             "menu": "Punch Auditing",
             Child: []
         },
         {
+            id: 15,
             "menu": "Rating Scale",
             Child: []
         },
         {
+            id: 15,
             "menu": "Recruiting",
             Child: []
         },
         {
+            id: 15,
             "menu": "Recruiting Setup",
             Child: []
         },
         {
+            id: 15,
             "menu": "Reward History Setup",
             Child: []
         },
         {
+            id: 15,
             "menu": "Schedule Setup",
             Child: [
                 "Job Assignment Activities",
@@ -276,78 +333,97 @@ let parentMenus =
             ]
         },
         {
+            id: 15,
             "menu": "Schedules",
             Child: []
         },
         {
+            id: 15,
             "menu": "Scheduling Plan",
             Child: []
         },
         {
+            id: 15,
             "menu": "Site Setup",
             Child: []
         },
         {
+            id: 15,
             "menu": "Succession",
             Child: []
         },
         {
+            id: 15,
             "menu": "Succession Setup",
             Child: []
         },
         {
+            id: 15,
             "menu": "System Admin",
             Child: []
         },
         {
+            id: 15,
             "menu": "Task Checklist",
             Child: []
         },
         {
+            id: 15,
             "menu": "Task Management",
             Child: []
         },
         {
+            id: 15,
             "menu": "Task Planning Calendar",
             Child: []
         },
         {
+            id: 15,
             "menu": "Task Setup",
             Child: []
         },
         {
+            id: 15,
             "menu": "Team Learning Activity",
             Child: []
         },
         {
+            id: 15,
             "menu": "Time Away From Work",
             Child: []
         },
         {
+            id: 15,
             "menu": "Time Review",
             Child: []
         },
         {
+            id: 15,
             "menu": "Timesheets",
             Child: []
         },
         {
+            id: 15,
             "menu": "Vacation Bidding",
             Child: []
         },
         {
+            id: 15,
             "menu": "Vacation Bidding Setup",
             Child: []
         },
         {
+            id: 15,
             "menu": "Work",
             Child: []
         },
         {
+            id: 15,
             "menu": "Workflow Administration",
             Child: []
         },
         {
+            id: 15,
             "menu": "Year End",
             Child: []
         }
@@ -382,7 +458,7 @@ $(document).ready(function () {
         if (i <= 14) {
 
             if (val.Child.length != 0) {
-                let final_str = str1 + "<a href=\"\" class=\"list-group-item list-group-item-action\">" + val.menu + "</a>" + str4;
+                let final_str = str1 + "<a id=\"" + i + 1 + "\" href=\"\" class=\"list-group-item list-group-item-action\" draggable=\"true\" ondragstart=\"drag(event)\">" + val.menu + "</a>" + str4;
                 $.each(val.Child, function (j, valc) {
                     final_str = final_str + "<a href=\"#\">" + valc + "</a>";
                 });
@@ -390,13 +466,13 @@ $(document).ready(function () {
                 menuArr1.push(final_str);
             }
             else {
-                menuArr1.push("<a href=\"\" class=\"list-group-item list-group-item-action\">" + val.menu + "</a>");
+                menuArr1.push("<a id=\"" + i + 1 + "\" href=\"\" class=\"list-group-item list-group-item-action\" draggable=\"true\" ondragstart=\"drag(event)\">" + val.menu + "</a>");
             }
         }
 
         if (i >= 15 && i <= 29) {
             if (val.Child.length != 0) {
-                let final_str = str1 + "<a href=\"\" class=\"list-group-item list-group-item-action\">" + val.menu + "</a>" + str4;
+                let final_str = str1 + "<a id=\"" + i + 1 + "\" href=\"\" class=\"list-group-item list-group-item-action\" draggable=\"true\" ondragstart=\"drag(event)\">" + val.menu + "</a>" + str4;
                 $.each(val.Child, function (j, valc) {
                     final_str = final_str + "<a href=\"#\">" + valc + "</a>";
                 });
@@ -404,14 +480,14 @@ $(document).ready(function () {
                 menuArr2.push(final_str);
             }
             else {
-                menuArr2.push("<a href=\"\" class=\"list-group-item list-group-item-action\">" + val.menu + "</a>");
+                menuArr2.push("<a id=\"" + i + 1 + "\" href=\"\" class=\"list-group-item list-group-item-action\" draggable=\"true\" ondragstart=\"drag(event)\">" + val.menu + "</a>");
             }
         }
 
 
         if (i >= 30 && i <= 44) {
             if (val.Child.length != 0) {
-                let final_str = str1 + "<a href=\"\" class=\"list-group-item list-group-item-action\">" + val.menu + "</a>" + str4;
+                let final_str = str1 + "<a id=\"" + i + 1 + "\" href=\"\" class=\"list-group-item list-group-item-action\" draggable=\"true\" ondragstart=\"drag(event)\">" + val.menu + "</a>" + str4;
                 $.each(val.Child, function (j, valc) {
                     final_str = final_str + "<a href=\"#\">" + valc + "</a>";
                 });
@@ -419,14 +495,14 @@ $(document).ready(function () {
                 menuArr3.push(final_str);
             }
             else {
-                menuArr3.push("<a href=\"\" class=\"list-group-item list-group-item-action\">" + val.menu + "</a>");
+                menuArr3.push("<a id=\"" + i + 1 + "\" href=\"\" class=\"list-group-item list-group-item-action\" draggable=\"true\" ondragstart=\"drag(event)\">" + val.menu + "</a>");
             }
 
         }
 
         if (i >= 45 && i <= 59) {
             if (val.Child.length != 0) {
-                let final_str = str1 + "<a href=\"\" class=\"list-group-item list-group-item-action\">" + val.menu + "</a>" + str4;
+                let final_str = str1 + "<a id=\"" + i + 1 + "\" href=\"\" class=\"list-group-item list-group-item-action\" draggable=\"true\" ondragstart=\"drag(event)\">" + val.menu + "</a>" + str4;
                 $.each(val.Child, function (j, valc) {
                     final_str = final_str + "<a href=\"#\">" + valc + "</a>";
                 });
@@ -434,14 +510,14 @@ $(document).ready(function () {
                 menuArr4.push(final_str);
             }
             else {
-                menuArr4.push("<a href=\"\" class=\"list-group-item list-group-item-action\">" + val.menu + "</a>");
+                menuArr4.push("<a id=\"" + i + 1 + "\" href=\"\" class=\"list-group-item list-group-item-action\" draggable=\"true\" ondragstart=\"drag(event)\">" + val.menu + "</a>");
             }
         }
 
 
         if (i >= 60 && i <= 75) {
             if (val.Child.length != 0) {
-                let final_str = str1 + "<a href=\"\" class=\"list-group-item list-group-item-action\">" + val.menu + "</a>" + str4;
+                let final_str = str1 + "<a id=\"" + i + 1 + "\" href=\"\" class=\"list-group-item list-group-item-action\" draggable=\"true\" ondragstart=\"drag(event)\">" + val.menu + "</a>" + str4;
                 $.each(val.Child, function (j, valc) {
                     final_str = final_str + "<a href=\"#\">" + valc + "</a>";
                 });
@@ -449,7 +525,7 @@ $(document).ready(function () {
                 menuArr5.push(final_str);
             }
             else {
-                menuArr5.push("<a href=\"\" class=\"list-group-item list-group-item-action\">" + val.menu + "</a>");
+                menuArr5.push("<a id=\"" + i + 1 + "\" href=\"\" class=\"list-group-item list-group-item-action\" draggable=\"true\" ondragstart=\"drag(event)\">" + val.menu + "</a>");
             }
         }
 
