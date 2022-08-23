32e36a55c370e52be3d4de04d26aad7d2430964a
deepeshbuckus <deepesh.buckus@gmail.com>
2022-08-23 14:07:30 +0400

    added home icon
    added home icon

--- parentMenu.js
+++ parentMenu.js
@@ -1,434 +1,434 @@
 let parentMenus =
-    [
-        {
-            id: 1,
-            "menu": "Analytics",
-            Child: []
-        },
-        {
-            id: 2,
-            "menu": "Anomaly Days",
-            Child: []
-        },
-        {
-            id: 3,
-            "menu": "Attendance Management",
-            Child: []
-        },
-        {
-            id: 4,
-            "menu": "Benefits",
-            Child: []
-        },
-        {
-            id: 5,
-            "menu": "Benefits Administration",
-            Child: []
-        },
-        {
-            id: 6,
-            "menu": "Benefits Intelligence",
-            Child: []
-        },
-        {
-            id: 7,
-            "menu": "Benefits Setup",
-            Child: []
-        },
-        {
-            id: 8,
-            "menu": "Career Profile Configuration",
-            Child: []
-        },
-        {
-            id: 9,
-            "menu": "Careers",
-            Child: []
-        },
-        {
-            id: 10,
-            "menu": "Centralized Schedules",
-            Child: []
-        },
-        {
-            id: 11,
-            "menu": "Compensation",
-            Child: []
-        },
-        {
-            id: 12,
-            "menu": "Compensation Administration",
-            Child: []
-        },
-        {
-            id: 13,
-            "menu": "Compensation Setup",
-            Child: []
-        },
-        {
-            id: 14,
-            "menu": "Data & Analytics",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Data Export Setup",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Documents",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Earnings",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Employee Timesheet",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Engagement",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Engagement Setup",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Getting Started",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "HCM Anywhere",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Home",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "HR Admin",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Learning",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Learning Setup",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Letter Management",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Manager Timesheet",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Market Pricing",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Mobile Org Schedules",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Pay Setup",
-            Child: [
-                "Minimum Wages",
-                "Clock Devices",
-                "Clock Device Group",
-                "Tips",
-                "Pay Holidays",
-                "Holiday Types",
-                "Pay Category",
-                "Pay Code",
-                "Pay Category Group",
-                "Holiday Group",
-                "Attendance",
-                "Pay Code Group",
-                "Multi-Approval",
-                "Balances",
-                "Base Rate Set",
-                "Entitlements",
-                "Job Rate",
-                "Pay Group",
-                "Pay Policy",
-                "Punch Policy",
-                "Long Service Leave",
-                "Period Authorization Policy",
-                "Time Review Administration"
-            ]
-        },
-        {
-            id: 15,
-            "menu": "Multi-Week Calendar",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Onboarding",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Onboarding Setup",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Operation View",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Org Setup",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Pay Admin Checklist",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Pay Approve Checklist",
-            Child: []
-        },
+    [{
+        id: 15,
+        "menu": "Home",
+        Child: []
+    },
+    {
+        id: 1,
+        "menu": "Analytics",
+        Child: []
+    },
+    {
+        id: 2,
+        "menu": "Anomaly Days",
+        Child: []
+    },
+    {
+        id: 3,
+        "menu": "Attendance Management",
+        Child: []
+    },
+    {
+        id: 4,
+        "menu": "Benefits",
+        Child: []
+    },
+    {
+        id: 5,
+        "menu": "Benefits Administration",
+        Child: []
+    },
+    {
+        id: 6,
+        "menu": "Benefits Intelligence",
+        Child: []
+    },
+    {
+        id: 7,
+        "menu": "Benefits Setup",
+        Child: []
+    },
+    {
+        id: 8,
+        "menu": "Career Profile Configuration",
+        Child: []
+    },
+    {
+        id: 9,
+        "menu": "Careers",
+        Child: []
+    },
+    {
+        id: 10,
+        "menu": "Centralized Schedules",
+        Child: []
+    },
+    {
+        id: 11,
+        "menu": "Compensation",
+        Child: []
+    },
+    {
+        id: 12,
+        "menu": "Compensation Administration",
+        Child: []
+    },
+    {
+        id: 13,
+        "menu": "Compensation Setup",
+        Child: []
+    },
+    {
+        id: 14,
+        "menu": "Data & Analytics",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Data Export Setup",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Documents",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Earnings",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Employee Timesheet",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Engagement",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Engagement Setup",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Getting Started",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "HCM Anywhere",
+        Child: []
+    },
 
-        {
-            id: 15,
-            "menu": "Payroll",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Payroll Import",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Payroll Setup",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Pension Auto-Enrollment",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Performance",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Performance Setup",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Period Review",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Schedule Setup",
-            Child: [
-                "Job Assignment Activities",
-                "Expression Token",
-                "Shift Trading Policy",
-                "Employee Order",
-                "Day Part",
-                "Activities",
-                "School Year",
-                "Schedule Rule Policy",
-                "Staff Assignment Policy",
-                "Assignment Policy",
-                "Autofill Run Profiles",
-                "Coverage Pane Setup",
-                "Employee Preferred Day Part",
-                "Employee Activities Rank Ordering",
-                "Event Administration",
-                "Event Properties",
-                "Operational Templates",
-                "Sales Statistics",
-                "Schedule Order",
-                "Skill Display",
-                "Schedule Change Policy",
-                "Work Contract Premium Policy"
-            ]
-        },
-        {
-            id: 15,
-            "menu": "Plan",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Plan Admin Checklist",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Plan Setup",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "PPACA",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Profile",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Projects",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Punch Auditing",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Rating Scale",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Recruiting",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Recruiting Setup",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Reward History Setup",
-            Child: []
-        },
+    {
+        id: 15,
+        "menu": "HR Admin",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Learning",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Learning Setup",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Letter Management",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Manager Timesheet",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Market Pricing",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Mobile Org Schedules",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Pay Setup",
+        Child: [
+            "Minimum Wages",
+            "Clock Devices",
+            "Clock Device Group",
+            "Tips",
+            "Pay Holidays",
+            "Holiday Types",
+            "Pay Category",
+            "Pay Code",
+            "Pay Category Group",
+            "Holiday Group",
+            "Attendance",
+            "Pay Code Group",
+            "Multi-Approval",
+            "Balances",
+            "Base Rate Set",
+            "Entitlements",
+            "Job Rate",
+            "Pay Group",
+            "Pay Policy",
+            "Punch Policy",
+            "Long Service Leave",
+            "Period Authorization Policy",
+            "Time Review Administration"
+        ]
+    },
+    {
+        id: 15,
+        "menu": "Multi-Week Calendar",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Onboarding",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Onboarding Setup",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Operation View",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Org Setup",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Pay Admin Checklist",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Pay Approve Checklist",
+        Child: []
+    },
 
-        {
-            id: 15,
-            "menu": "Schedules",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Scheduling Plan",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Site Setup",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Succession",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Succession Setup",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "System Admin",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Task Checklist",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Task Management",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Task Planning Calendar",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Task Setup",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Team Learning Activity",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Time Away From Work",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Time Review",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Timesheets",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Vacation Bidding",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Vacation Bidding Setup",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Work",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Workflow Administration",
-            Child: []
-        },
-        {
-            id: 15,
-            "menu": "Year End",
-            Child: []
-        }
+    {
+        id: 15,
+        "menu": "Payroll",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Payroll Import",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Payroll Setup",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Pension Auto-Enrollment",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Performance",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Performance Setup",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Period Review",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Schedule Setup",
+        Child: [
+            "Job Assignment Activities",
+            "Expression Token",
+            "Shift Trading Policy",
+            "Employee Order",
+            "Day Part",
+            "Activities",
+            "School Year",
+            "Schedule Rule Policy",
+            "Staff Assignment Policy",
+            "Assignment Policy",
+            "Autofill Run Profiles",
+            "Coverage Pane Setup",
+            "Employee Preferred Day Part",
+            "Employee Activities Rank Ordering",
+            "Event Administration",
+            "Event Properties",
+            "Operational Templates",
+            "Sales Statistics",
+            "Schedule Order",
+            "Skill Display",
+            "Schedule Change Policy",
+            "Work Contract Premium Policy"
+        ]
+    },
+    {
+        id: 15,
+        "menu": "Plan",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Plan Admin Checklist",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Plan Setup",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "PPACA",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Profile",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Projects",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Punch Auditing",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Rating Scale",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Recruiting",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Recruiting Setup",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Reward History Setup",
+        Child: []
+    },
+
+    {
+        id: 15,
+        "menu": "Schedules",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Scheduling Plan",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Site Setup",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Succession",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Succession Setup",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "System Admin",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Task Checklist",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Task Management",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Task Planning Calendar",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Task Setup",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Team Learning Activity",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Time Away From Work",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Time Review",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Timesheets",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Vacation Bidding",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Vacation Bidding Setup",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Work",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Workflow Administration",
+        Child: []
+    },
+    {
+        id: 15,
+        "menu": "Year End",
+        Child: []
+    }
     ];
 
 let orderedPerentMenu = [{
